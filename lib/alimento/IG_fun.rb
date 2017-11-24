498421ebc0205c990b07428baa66f1cebd3f6edb
Christian <alu0100886306@ull.edu.es>
2017-11-24 12:38:27 +0000

    Prueba travis

--- lib/alimento/IG_fun.rb
+++ lib/alimento/IG_fun.rb
@@ -4,18 +4,12 @@ def ig_func (vpruebas,vglucosas)
 			|prueba| (prueba-pruebas[0])*5-(pruebas[pruebas.length-1]-pruebas[0])*5/(2*pruebas.length)
 		end.reduce('+')
 	end #Array con los AIBC de cada prueba
-	puts a.to_s
 	b=vglucosas.collect do 
 		|glucosas| glucosas.collect do 
 			|glucosa| (glucosa-glucosas[0])*5-(glucosas[glucosas.length-1]-glucosas[0])*5/(2*glucosas.length)
 		end.reduce('+')
 	end #Array con los AIBC de cada glucosa
-	puts b.to_s	
-	i=-1
-	ig=a.collect do 
-		|aux| i+=1
-		(aux*100)/b[i] 		
-	end.reduce('+')
+	ig=a.zip(b).collect { |v1,v2| v1*100/v2 }.reduce('+') 
 	ig/=vpruebas.length #IG
 	return ig
 end
