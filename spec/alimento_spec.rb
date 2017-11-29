396650452246d8ecce9ba2b22c89cec1c244cac2
Christian <alu0100886306@ull.edu.es>
2017-11-29 10:12:34 +0000

    Creadas pruebas de las ordenaciones

--- spec/alimento_spec.rb
+++ spec/alimento_spec.rb
@@ -2,8 +2,9 @@ require "spec_helper"
  
 RSpec.describe Alimento do
   before :all do 
-		@a1=Alimento::Alimento.new("Huevo frito", 14.1, 0.0, 19.5)
-		@a2=Alimento::Alimento.new("Leche",       3.3,  4.8, 3.2)
+		@a1=Alimento::Alimento.new("Huevo frito", 14.1,  0.0, 19.5)
+		@a2=Alimento::Alimento.new("Leche",        3.3,  4.8,  3.2)
+		@a3=Alimento::Alimento.new("Mantequilla" , 0.7,  0.0, 83.2)
 		@n1=Nodo.new(@a2,nil,nil)
 		@n2=Nodo.new(@a1,nil,nil)
 		@l1=Lista.new
@@ -94,6 +95,18 @@ RSpec.describe Alimento do
 		it "Se clacula correctamente" do
 			expect(@a1.kcal).to eq(231.9)
 		end	
+	end
+
+	describe "# Ordenación en base al valor energético" do
+		it "Con for" do
+			expect(ord_for(@a1,@a2,@a3)).to eq([@a1,@a2,@a3])
+		end
+		it "Con each" do
+			expect(ord_each(@a1,@a2,@a3)).to eq([@a1,@a2,@a3])
+		end
+		it "Con sort" do
+			expect(ord_sort(@a1,@a2,@a3)).to eq([@a1,@a2,@a3])
+		end
 	end
 	
 end
